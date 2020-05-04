3a27558e43cbabf505a0d8cba653291ac89164d0
Marcel Król <marcelkrol99@gmail.com>
2020-05-04 14:46:15 +0200

    Initial Commit

---
+++ Snake/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Snake
+//
+//  Created by Marcel on 04/05/2020.
+//  Copyright © 2020 Marcel. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
